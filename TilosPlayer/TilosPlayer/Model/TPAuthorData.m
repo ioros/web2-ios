9dc9cc7a595f7d2b1c15ce360e7f0355a9625d91
rumori <intonarumori@gmail.com>
2014-10-24 01:36:05 +0200

    removed real names

--- TilosPlayer/TilosPlayer/Model/TPAuthorData.m
+++ TilosPlayer/TilosPlayer/Model/TPAuthorData.m
@@ -17,11 +17,11 @@
     if(object == nil) return nil;
     
     // TODO: remove this if server is fixed
-    NSString *name = [object objectForKeyOrNil:@"name"];
-    name = [name stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    //NSString *name = [object objectForKeyOrNil:@"name"];
+    //name = [name stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
     
     TPAuthorData *data = [TPAuthorData new];
-    data.name = name;
+    //data.name = name;
     data.identifier = [object objectForKey:@"id"];
     data.avatarURL = [object objectForKey:@"avatar"];
     data.photoURL = [object objectForKey:@"photo"];
